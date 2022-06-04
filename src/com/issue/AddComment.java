2636ad7dea5132008e21f8a2a391ac1b2eb242eb
naveenkurella8@gmail.com <naveenkurella8@gmail.com>
2022-06-04 15:16:53 +0530

    added syso lines

--- src/com/issue/AddComment.java
+++ src/com/issue/AddComment.java
@@ -14,6 +14,9 @@ public class AddComment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
+		System.out.println("Test test");
+		System.out.println("Test test");
+		System.out.println("Test test");
 		RestAssured.baseURI = "http://localhost:8080";
 		SessionFilter filter=new SessionFilter();
 		String response = given().log().all().header("Content-Type", "application/json")
